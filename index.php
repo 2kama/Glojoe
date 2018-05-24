6f9e829513a3e33f345cc016b2eb23abf217e566
Mishael Kama <mishealkama@gmail.com>
2018-05-24 14:41:27 +0100

    1st commit

---
+++ index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+    
+    <script src="js/jquery.js" crossorigin="anonymous"></script>
+
+
+
+    <title>Glo with Joe</title>
+  </head>
+  <body>
+
+
+
+ <div class="container-fluid bean" style="background: url('img/back.png') no-repeat center center / cover">
+ 	
+	<div class="row">
+		
+		<div class="col-md-6 left">
+			<div class="col-md-12 text-center" style="margin-top: 20%;">
+				<img src="img/logo.png" alt="">
+			</div>
+
+			<div class="row">
+				<div class="col-md-6 offset-md-3" style="margin-top: 70px;">
+					<h6 class="welcome col-md-12">Welcome</h6>
+
+					<form action="" style="margin-top: 15px;" class="col-md-12">
+						<input type="email" class="form-control" placeholder="Your email address">
+						<input type="password" class="form-control" placeholder="Your password">
+
+						<button type="submit" class="btn btn-block">Sign in</button>
+					</form>
+
+					<div class="col-md-6 offset-md-6 forgot">
+						forgot password?
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-6 right">
+			
+			<div class="row">
+				<div class="col-md-8 offset-md-2" style="margin-top: 20%;">
+					<h5 class="stain">GLO WITH JOE</h5>
+					<p class="pass">
+						JOE makes every effort to provide optimal security of patient health <br> 
+						related data and all of its transactions, for us protecting our <br>
+						clients data is of utmost importance.
+					</p>
+				</div>
+			</div>
+
+			<div class="row" style="margin-top: 50px;">
+				
+				<div class="col-md-4 offset-md-2 sax">
+					<img src="img/01.png" alt="">
+					<h6>Easy to use</h6>
+					<p>
+						Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit. Cras <br> efficitur scelerisque malesuada.
+				    </p>
+				</div>
+
+				<div class="col-md-4 sax">
+					<img src="img/02.png" alt="">
+					<h6>Quick registration process</h6>
+					<p>
+						Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit. Cras <br> efficitur scelerisque malesuada.
+				    </p>
+				</div>
+
+				<div class="col-md-4 offset-md-2 sax bottom_img">
+					<img src="img/03.png" alt="">
+					<h6>Easy access to doctors</h6>
+					<p>
+						Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit. Cras <br> efficitur scelerisque malesuada.
+				    </p>
+				</div>
+
+				<div class="col-md-4 sax bottom_img">
+					<img src="img/04.png" alt="">
+					<h6>Pharmaceutical</h6>
+					<p>
+						Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit. Cras <br> efficitur scelerisque malesuada.
+				    </p>
+				</div>
+
+			</div>
+
+		</div>
+
+
+		<div class="col-md-12 foot">
+			Powered by <img src="img/sbsc.png" alt="">
+		</div>
+
+	</div>
+
+ </div>
+
+
+
+
+
+
+<style>
+	.foot {
+		position: fixed;
+		bottom: 5px;
+		z-index: 20;
+		text-align: right;
+		padding-right: 100px;
+	}
+	.sax {
+		margin-bottom: 40px;
+	}
+	.sax h6 {
+		margin-top: 7px;
+	}
+	.sax p {
+		margin-top: 7px;
+		color: #9b9b9b;
+		font-size: 14px;
+	}
+	.pass {
+		color: #9b9b9b;
+		font-size: 14px;
+	}
+	.stain {
+		color: #1f282e;
+		font-size: 20px;
+		font-weight: bold;
+	}
+	.bean {
+		padding: 0;
+		margin: 0;
+		height: 100vh;
+		overflow-y: auto;
+		width: 100%;
+	}
+	.left {
+		 background: rgba(255, 255, 255, 0.7);
+		 min-height: 100vh;
+		
+	}
+	.right {
+		 background: #ffffff;
+		 min-height: 100vh;
+		 
+	}
+	.bean > .row {
+		width: 100%;
+		height: 100vh;
+		position: fixed;
+		overflow-y: auto;
+	}
+	h6.welcome {
+		font-size: 20px;
+		color:#051835;
+	}
+	input.form-control {
+		border-radius: 0px;
+		-webkit-border-radius: 0px;
+		border: 1px solid #ebebf2;
+		height: 50px;
+	}
+	input.form-control:focus {
+		box-shadow: 0 0 0 rgba(0,0,0,0);
+	}
+	input.form-control::placeholder {
+		color: #b1bac8;
+		font-size: 14px;
+	}
+	button {
+		margin-top: 20px;
+		background: #3c9b34;
+		color: #fff;
+		font-size: 14px;
+		height: 50px;
+
+	}
+	button:hover {
+		opacity: 0.8;
+	}
+	.forgot {
+		font-size: 13px;
+		text-align: right;
+		color: #9b9b9b;
+		cursor: pointer;
+		margin-top: 10px;
+	}
+	p {
+		font-weight: 300;
+	}
+</style>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/popper.js"></script>
+
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,95 @@
+
+
+$(document).ready(function () {
+    
+    $('.notification .clean').click(function() {
+
+        $('.notification').fadeOut();
+    });
+
+
+
+
+    $('.tele1').click(function() {
+        
+        $('.sas .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $('.sandal').hide();
+        $('.form1').show();
+
+    });
+
+
+
+    $('.tele2').click(function() {
+        
+        $('.sas .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $('.sandal').hide();
+        $('.form2').show();
+
+    });
+
+
+
+    $('.tele3').click(function() {
+        
+        $('.sas .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $('.sandal').hide();
+        $('.form3').show();
+
+    });
+
+
+
+    $('.tele4').click(function() {
+        
+        $('.sas .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $('.sandal').hide();
+        $('.form4').show();
+
+    });
+
+
+
+    $('.tele5').click(function() {
+        
+        $('.sas .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $('.sandal').hide();
+        $('.form5').show();
+
+    });
+
+
+
+    $('.tele6').click(function() {
+        
+        $('.sas .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $('.sandal').hide();
+        $('.form6').show();
+
+    });
+
+
+
+    $('.tele7').click(function() {
+        
+        $('.sas .list-group-item').removeClass("active");
+        $(this).addClass("active");
+
+        $('.sandal').hide();
+        $('.form7').show();
+
+    });
+
+});
