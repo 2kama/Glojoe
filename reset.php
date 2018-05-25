22cc7cc9dca7b808fcd06c27e2297ee7d6b26a5d
Mishael Kama <mishealkama@gmail.com>
2018-05-25 11:54:27 +0100

    2nd commit

---
+++ reset.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+    
+    <script src="js/jquery.js" crossorigin="anonymous"></script>
+
+
+
+    <title>Glo with Joe</title>
+  </head>
+  <body>
+
+
+
+ <div class="container-fluid bean" style="background: url('img/back.png') no-repeat center center / cover">
+ 	
+	<div class="row" style="background: rgba(255, 255, 255, 0.8);height: 100vh;">
+		
+		<div class="col-md-12 text-center">
+			<div class="col-md-12" style="margin-top: 120px;">
+				<img src="img/logo.png" alt="">
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 offset-md-4" style="margin-top: 70px;">
+					<h6 class="welcome col-md-12">Forgot your password ?</h6>
+					<p>We’ll email you instructions oh how to reset it</p>
+
+					<form action="" style="margin-top: 25px;" class="col-md-12">
+						<input type="email" class="form-control" placeholder="Your email address">
+
+						<button type="submit" class="btn btn-block">Sign in</button>
+					</form>
+
+					<div class="col-md-12 forgot">
+						back to <a href="index">Login</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	
+
+
+		<div class="col-md-12 foot">
+			Powered by <img src="img/sbsc.png" alt="">
+		</div>
+
+	</div>
+
+ </div>
+
+
+
+
+
+
+<style>
+	.foot {
+		position: fixed;
+		bottom: 5px;
+		z-index: 20;
+		text-align: right;
+		padding-right: 100px;
+	}
+	
+	.pass {
+		color: #9b9b9b;
+		font-size: 14px;
+	}
+	.stain {
+		color: #1f282e;
+		font-size: 20px;
+		font-weight: bold;
+	}
+	.bean {
+		padding: 0;
+		margin: 0;
+		height: 100vh;
+		overflow-y: auto;
+		width: 100%;
+	}
+	
+		 
+	}
+	.bean > .row {
+		width: 100%;
+		height: 100vh;
+		position: fixed;
+		overflow-y: auto;
+	}
+	h6.welcome {
+		font-size: 20px;
+		color:#051835;
+	}
+	input.form-control {
+		border-radius: 0px;
+		-webkit-border-radius: 0px;
+		border: 1px solid #ebebf2;
+		height: 50px;
+	}
+	input.form-control:focus {
+		box-shadow: 0 0 0 rgba(0,0,0,0);
+	}
+	input.form-control::placeholder {
+		color: #b1bac8;
+		font-size: 14px;
+	}
+	button {
+		margin-top: 20px;
+		background: #3c9b34;
+		color: #fff;
+		font-size: 14px;
+		height: 50px;
+
+	}
+	button:hover {
+		opacity: 0.8;
+	}
+	.forgot {
+		font-size: 13px;
+		text-align: center;
+		color: #9b9b9b;
+		cursor: pointer;
+		margin-top: 10px;
+	}
+	.forgot a {
+		color: #3c9b34;
+	}
+	p {
+		font-weight: 300;
+	}
+</style>
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/popper.js"></script>
+
+
+</body>
+</html>
